725bdc38b28c0f84bde447fe0fc5421c33621f8c
Patrick Burke <W0480974@nscc.ca>
2023-09-26 10:37:42 -0300

    First Version

---
+++ Server/Server.js
@@ -0,0 +1,38 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({
+  port: 8080,
+});
+
+let player_index = 0;
+
+// function pingPlayer() {
+//   server.clients.forEach(function each(client) {
+//     client.send("Hello");
+//   });
+// }
+
+let players = [];
+server.on('connection', function(socket) {
+    players[player_index] = socket;
+    player_index++;
+
+    //SEND PLAYER HIS PLAYER INDEX HERE, DESIGN MESSAGING SYSTEM SERVER->CLIENT
+
+    console.log(`Client connected! (${players.length})`);
+
+    //When message received from a client
+    socket.on('message', function(event) {
+        //sockets.forEach(s => s.send(msg));
+        const msg = JSON.parse(event);
+        console.log(`${players.length} players connected.`);
+        console.log(`PLAYER ${msg.net_name}: X: ${msg.net_x} Y: ${msg.net_y} dX: ${msg.net_dx} dY: ${msg.net_dy}`);
+  });
+
+  // When a socket closes, or disconnects, remove it from the array.
+    socket.on('close', function() {
+        players = players.filter(s => s !== socket);
+        console.log("Client Disconnected!");
+  });
+});
+
+// setInterval(pingPlayer, 10000);
---
+++ game.js
@@ -0,0 +1,100 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const ws = new WebSocket("ws://localhost:8080");
+
+let draw_process, net_process;
+let is_connected = false;
+
+let x = canvas.width/2;
+let y = canvas.height/2;
+
+let move_speed = 2;
+let dx = 0;
+let dy = 0;
+
+//Extract the player name from the url arguments
+let url = window.location.search;
+let url_arguments = url.split("=");
+const player_name = url_arguments[1];
+
+//INPUT EVENTS GO HERE
+let map = {}; // You could also use an array
+onkeydown = function(e){
+    if (is_connected) {
+        map[e.key] = e.type == 'keydown';
+        if (map["a"]) {
+            dx = -1;
+        }
+        if(map["d"]) {
+            dx = 1;
+        }
+        if(map["s"]) {
+            dy = 1;
+        }
+        if(map["w"]) {
+            dy =- 1;
+        }
+    }
+}
+
+onkeyup = function(e){
+    if (is_connected) {
+        map[e.key] = e.type == 'keydown';
+        if (!map["a"] || !map["d"]) {
+            dx = 0;
+        }
+    
+        if(!map["s"] || !map["w"]) {
+            dy = 0;
+        }
+    }
+}
+
+//NETWORKED EVENTS GO HERE
+ws.onopen = (event => {
+    draw_process = setInterval(draw, 10);
+    net_process = setInterval(sendLocation, 2000);
+    is_connected = true;
+});
+
+ws.onclose = (event => {
+    clearInterval(net_process);
+    clearInterval(draw_process);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    alert("You have been disconnected from the server!")
+    is_connected = false;
+});
+
+ws.onmessage = (message) => {
+    // alert("Data received from server!");
+}
+
+function sendLocation() {
+    const player_data = {
+        net_name: player_name,
+        net_x: x,
+        net_y: y,
+        net_dx: dx,
+        net_dy: dy,
+    };
+
+    ws.send(JSON.stringify(player_data));
+}
+
+//DRAWING CODE GOES HERE
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+    x += dx;
+    y += dy;
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+}
